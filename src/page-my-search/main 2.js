7c6c25ec8264822b7f8284bf000e53f410d740af
zhanwei <@zhanwei.dzw>
2020-06-28 17:27:56 +0800

    145更新

---
+++ src/page-my-search/main 2.js
@@ -0,0 +1,33 @@
+import {Component} from 'react'
+import {observer} from 'mobx-react'
+import {
+  HashRouter as Router, Route, Switch,
+} from 'react-router-dom'
+
+import Frame from '../frame'
+
+import MySearch from './my-search'
+import Tql from './search-detail-tql'
+import Visual from './search-detail-visual'
+
+@observer
+export default class Page extends Component {
+  render() {
+    return (
+      <Router>
+        <Frame page="space" roductCode="stream" theme="ocean" logoText="数据开发" showAllProduct showSider showHeaderNav showProject>
+          <Switch>
+            <Route exact path="/my-search" component={MySearch} />
+            <Route exact path="/my-search/tql/:id" component={Tql} />
+            <Route exact path="/my-search/visual/:id" component={Visual} />
+            <Route
+              render={() => {
+                window.location.href = '/404'
+              }}
+            />
+          </Switch>
+        </Frame>
+      </Router>
+    )
+  }
+}
