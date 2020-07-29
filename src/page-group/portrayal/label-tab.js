a0b438428b56b61c62978124492828f9467baf45
zhanwei <@zhanwei.dzw>
2020-07-29 15:55:10 +0800

    微观画像bug修复，组件升级

--- src/page-group/portrayal/label-tab.js
+++ src/page-group/portrayal/label-tab.js
@@ -1,7 +1,7 @@
-import React, {Component, Fragment} from 'react'
-import {action, observe, toJS} from 'mobx'
+import React, {Component} from 'react'
+import {toJS, action} from 'mobx'
 import {observer, inject} from 'mobx-react'
-import {Select, Button, Tooltip, Progress, Spin} from 'antd'
+import {Button, Tooltip, Progress} from 'antd'
 import {TagFilled} from '@ant-design/icons'
 
 import {NoData} from '../../component'
@@ -14,17 +14,6 @@ export default class LabelTab extends Component {
     super(props)
     this.store = props.store
   }
-  // componentWillUpdate() {
-
-  // }
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.store.tooltipX !== this.props.tooltipX) {
-      const {tooltipX, tooltipY} = this.store
-      // this.getDom(tooltipX, tooltipY)
-      console.log(tooltipX, tooltipY)
-    }
-  }
 
   // 判断是否是重复的请求
   @action isRepeat = nowTag => {
@@ -36,86 +25,66 @@ export default class LabelTab extends Component {
     tagAnalysis(nowTag)
   }
 
-  // 生成dom结构
-  @action getDom = (x, y) => {
-    const domList = []
-    const {labelRes} = this.store
-    for (let i = 0; i < labelRes.length; i++) {
-      const nowRes = labelRes[i].tags || []
-      const nowCategoryName = labelRes[i].categoryName || []
-      
-      // 生成单个提示标签
-      this.store.itemLabels = toJS(nowRes).map((item, index) => {
-        return (
-          <Tooltip 
-            key={item} 
-            title={(
-              <div>
-                <div>
-                  {/* <span>{toJS(this.store.tooltipX)}</span> */}
-                  <span>{x}</span>
-                </div>
-                <Progress 
-                  showInfo
-                  status="active"
-                  strokeWidth={4} 
-                  strokeColor="#00d5af" 
-                  // percent={parseInt(toJS(this.store.tooltipY), 10)} 
-                  percent={parseInt(y, 10)} 
-                  color="#fff"
-                  style={{color: '#fff', width: '96px', marginRight: '8px'}}
-                />
-              </div>
-            )}
-            color="rgba(0,0,0,.65)" 
-          >
-            <Button 
-              className="label-btn"
-              // onMouseEnter={() => this.isRepeat(nowRes[index])}
-              onMouseEnter={() => debounce(() => this.isRepeat(nowRes[index]), 200)}
-            >
-              {item.value}
-            </Button>
-          </Tooltip>
-        )
-      })
-
-      // 生成段落标签标题
-      domList.push(
-        <div className="tab-content">
-          <div>
-            <TagFilled rotate={270} style={{color: 'rgba(0,0,0,.65)', marginRight: '12px'}} />
-            <span>{`${nowCategoryName}（${nowRes.length}）`}</span>
-          </div>
-          {this.store.itemLabels}
-        </div>
-      )
-    }
-    return domList
-  }
-
   render() {
-    const {labelRes, tooltipX, tooltipY, markedLoading, domList} = this.store
-    console.log(toJS(labelRes))
+    const {labelRes, tooltipX, tooltipY} = this.store
     return (
-      // <Spin spinning={markedLoading}>
       <div className="pl24 pt-0">
         {
           labelRes.length ? (
-            domList
+            labelRes.map(now => {
+              const nowRes = now.tags || []
+              const nowCategoryName = now.categoryName || []
+              
+              // 生成单个提示标签
+              this.itemLabels = toJS(nowRes).map((item, index) => {
+                return (
+                  <Tooltip 
+                    key={item.value} 
+                    title={(
+                      <div>
+                        <div>
+                          <span>{tooltipX}</span>
+                          {console.log(tooltipX)}
+                        </div>
+                        <Progress 
+                          showInfo
+                          status="active"
+                          strokeWidth={4} 
+                          strokeColor="#00d5af" 
+                          percent={parseInt(tooltipY, 10)} 
+                          color="#fff"
+                          style={{color: '#fff', width: '96px', marginRight: '8px'}}
+                        />
+                      </div>
+                    )}
+                    color="rgba(0,0,0,.65)" 
+                  >
+                    <Button 
+                      className="label-btn"
+                      onMouseEnter={() => debounce(() => this.isRepeat(nowRes[index]), 200)}
+                    >
+                      {item.value}
+                    </Button>
+                  </Tooltip>
+                )
+              })
+        
+              // 生成段落标签标题
+              return (
+                <div className="tab-content">
+                  <div>
+                    <TagFilled rotate={270} style={{color: 'rgba(0,0,0,.65)', marginRight: '12px'}} />
+                    <span>{`${nowCategoryName}（${nowRes.length}）`}</span>
+                  </div>
+                  {this.itemLabels}
+                </div>
+              )
+            })
           ) : (
             <NoData />
           )
         }
-        {/* {
-          labelRes.length ? (
-            this.getDom(tooltipX, tooltipY)
-          ) : (
-            <NoData />
-          )
-        } */}
       </div>
-      // </Spin>
     )
   }
 }
