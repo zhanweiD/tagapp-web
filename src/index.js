dc5ee8a1c10283d218427ec4114588aff41f9e8d
qian.wq <qian.wq@dtwave-inc.com>
2020-08-13 14:57:46 +0800

    顶部导航更改

--- src/index.js
+++ src/index.js
@@ -24,12 +24,12 @@ window.njkData = njkData
 
 const quickEntrance = [
   {
-    label: '后台配置',
+    tip: '后台配置',
     icon: 'setting',
     url: '/tag-model/index.html#/config/environment',
   },
   {
-    label: '审批管理',
+    tip: '审批管理',
     icon: 'approver',
     url: '/tag-model/index.html#/common/approval',
   },
