c7f5a3613c8ff642dea90e131497fc1bfc2bdb4e
qian.wq <qian.wq@dtwave-inc.com>
2020-09-02 15:43:18 +0800

    群体优化

--- src/page-group/group-analyze/main.js
+++ src/page-group/group-analyze/main.js
@@ -4,7 +4,7 @@
 import {Component} from 'react'
 import {observer, Provider} from 'mobx-react'
 import {action} from 'mobx'
-import {Button, Popconfirm} from 'antd'
+import {Button, Popconfirm, Spin} from 'antd'
 import {projectProvider, NoData, groupProvider} from '../../component'
 import Search from './search'
 import ModalAdd from './modal'
@@ -92,36 +92,37 @@ class GroupAnalyze extends Component {
   }
 
   render() {
-    const {roportion, info, groupId} = store
+    const {roportion, info, groupId, roportionLoading} = store
     const {match: {params}} = this.props
 
     return (
       <Provider store={store}>
-        <div>
-          <div className="analyze-search">
-            <span>群体分析</span>
-            {
-              !params.groupId ? <Search search={this.search} /> : null
-            }
-          </div>
-
-          <div style={{display: roportion.time ? 'block' : 'none'}} className="analyze-roportion box-border">
-            {
-              params.groupId ? <div className="analyze-roportion-name">{roportion.groupName}</div> : null
-            }
-            <div ref={ref => this.roportionRef = ref} style={{height: '150px', width: '150px'}} />
-            <div className="analyze-roportion-text">
-              <div className="fs24">{roportion.groupCount}</div>
-              <div>{`在全部实体 ${roportion.totalCount} 个中占比${roportion.totalCount ? (roportion.groupCount / roportion.totalCount * 100).toFixed(2) : 0}%`}</div>
-            
+        <Spin spinning={roportionLoading} tip="查询中，请稍后...">
+          <div>
+            <div className="analyze-search">
+              <span>群体分析</span>
+              {
+                !params.groupId ? <Search search={this.search} /> : null
+              }
             </div>
-            <div className="analyze-roportion-time">
-              <span>计算于：</span>
-              {moment(+roportion.time).format('YYYY-MM-DD HH:mm:ss')}
-            </div>
-          </div>
 
-          {/* {
+            <div style={{ display: roportion.time ? 'block' : 'none' }} className="analyze-roportion box-border">
+              {
+                params.groupId ? <div className="analyze-roportion-name">{roportion.groupName}</div> : null
+              }
+              <div ref={ref => this.roportionRef = ref} style={{ height: '150px', width: '150px' }} />
+              <div className="analyze-roportion-text">
+                <div className="fs24">{roportion.groupCount}</div>
+                <div>{`在全部实体 ${roportion.totalCount} 个中占比${roportion.totalCount ? (roportion.groupCount / roportion.totalCount * 100).toFixed(2) : 0}%`}</div>
+
+              </div>
+              <div className="analyze-roportion-time">
+                <span>计算于：</span>
+                {moment(+roportion.time).format('YYYY-MM-DD HH:mm:ss')}
+              </div>
+            </div>
+
+            {/* {
             groupId ? (
               <div className="analyze-action mb12 mt12">
                 <span>群体画像</span>
@@ -129,55 +130,56 @@ class GroupAnalyze extends Component {
               </div>
             ) : null
           } */}
-         
-          <div className="group-analyze header-page">
-            {
-              groupId ? (
-                <div className="analyze-action bgf content-header">
-                  <span>群体画像</span>
-                  <Button type="primary" className="mt16" onClick={this.showModal} disable={info.length === 20}>添加分析纬度</Button>
-                </div>
-              ) : null
-            }
-            <div className="chart-content">
-              {
 
-                info.length
-                  ? info.map(({
-                    Comp,
-                    tagName,
-                    tagId,
-                    ...rest
-                  }, index) => (
-                    <div className="chart-wrap">
-                      <div className="chart-box">
-                        <div className="chart-item-title">
-                          <span>{tagName}</span>
-                          <div className="FBH">
-                            <IconEdit size="14" onClick={() => this.edit({tagId, ...rest}, index)} className="mr8 mt8 mb16 action" />
-                            <Popconfirm
-                              placement="bottomLeft"
-                              title="确定要删除吗？"
-                              onConfirm={() => this.del(rest, index)}
-                              okText="确认"
-                              cancelText="取消"
-                            >
-                              <IconDel size="14" className="mt8 action" />
-                            </Popconfirm>
-                          
+            <div className="group-analyze header-page">
+              {
+                groupId ? (
+                  <div className="analyze-action bgf content-header">
+                    <span>群体画像</span>
+                    <Button type="primary" className="mt16" onClick={this.showModal} disable={info.length === 20}>添加分析纬度</Button>
+                  </div>
+                ) : null
+              }
+              <div className="chart-content">
+                {
+
+                  info.length
+                    ? info.map(({
+                      Comp,
+                      tagName,
+                      tagId,
+                      ...rest
+                    }, index) => (
+                        <div className="chart-wrap">
+                          <div className="chart-box">
+                            <div className="chart-item-title">
+                              <span>{tagName}</span>
+                              <div className="FBH">
+                                <IconEdit size="14" onClick={() => this.edit({ tagId, ...rest }, index)} className="mr8 mt8 mb16 action" />
+                                <Popconfirm
+                                  placement="bottomLeft"
+                                  title="确定要删除吗？"
+                                  onConfirm={() => this.del(rest, index)}
+                                  okText="确认"
+                                  cancelText="取消"
+                                >
+                                  <IconDel size="14" className="mt8 action" />
+                                </Popconfirm>
+
+                              </div>
+                            </div>
+                            <Comp data={{ tagName, ...rest }} key={`${tagId}${rest.chartType}${rest.groupType || 0}`} />
                           </div>
                         </div>
-                        <Comp data={{tagName, ...rest}} key={`${tagId}${rest.chartType}${rest.groupType || 0}`} />
-                      </div>
-                    </div>
-                  ))
-                  // className="header-page" style={{margin: '0px 8px 16px'}}
-                  : <div><NoData text={roportion.time ? '请添加分析纬度' : '请选择目标群体，完成群体分析'} style={{marginTop: '15%'}} /></div>
-              }
+                      ))
+                    // className="header-page" style={{margin: '0px 8px 16px'}}
+                    : <div><NoData text={roportion.time ? '请添加分析纬度' : '请选择目标群体，完成群体分析'} style={{ marginTop: '15%' }} /></div>
+                }
+              </div>
+              <ModalAdd add={this.add} />
             </div>
-            <ModalAdd add={this.add} />
           </div>
-        </div>
+        </Spin>
       </Provider>
     )
   }
